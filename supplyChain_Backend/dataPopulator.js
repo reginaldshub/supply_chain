8d6d86dd7ddd0c496e43ad5fc6d840d64911ca29
reginaldshub <reginald.anthony@accionlabs.com>
2019-06-24 15:31:19 +0530

    node script

---
+++ supplyChain_Backend/dataPopulator.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const KeyManager = require("./routes/keymanager");
+var User = require("./models/user");
+let rawdata = fs.readFileSync('data.json');
+let student = JSON.parse(rawdata);
+// console.log(student.Farmer);
+var keyManager = new KeyManager();
+
+student.Farmer.forEach((farmer) => {
+    var output = keyManager.createkeys(farmer.email);
+    keyManager.savekeys(farmer.email, output);
+
+})
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("CrudDB");
+    dbo.collection("users").insertMany(student.Farmer, { useNewUrlParser: true }, function(err, res) {
+        if (err) throw err;
+        dbo.collection("users").insertMany(student.Inspector, { useNewUrlParser: true }, function(err, res) {
+            if (err) throw err;
+            dbo.collection("users").insertMany(student.ProcessAgent, { useNewUrlParser: true }, function(err, res) {
+                if (err) throw err;
+                dbo.collection("users").insertMany(student.RetailAgent, { useNewUrlParser: true }, function(err, res) {
+                    if (err) throw err;
+                    db.close();
+                });
+            });
+        });
+
+
+    });
+});
